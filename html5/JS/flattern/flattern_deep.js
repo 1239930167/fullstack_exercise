3009d38edfcea87cede5d91d2556e4ef7ba4bf09
1239930167 <1239930167@qq.com>
2018-06-20 23:50:20 +0800

    js展平数组

---
+++ html5/JS/flattern/flattern_deep.js
@@ -0,0 +1,10 @@
+function flatten(arr){
+    // reduce() 削藩：将前一项与前一项做操作   es6用法
+    return arr.reduce(function(prev, next) {
+        console.log(prev, next);
+        
+        return prev.concat(Array.isArray(next)?flattern(next):next);
+    }, [])
+}
+
+console.log(flatten([1,2,[3,4],[5,[6,7]]]));
